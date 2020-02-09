868d536767afdb298d5cb5553dbf1e21548d6949
Rich Jiang <rich.jiang2@gmail.com>
2020-02-08 21:37:56 -0500

    made edit account page

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import {SetProfile} from "../actions/authActions";
 import {connect} from "react-redux";
+import {Form, Checkbox, Button, Message} from "semantic-ui-react";
+import {EDIT_ACCOUNT} from "../routing/routes";
 //Rich owns this one
 
 const Account = (props) => {
     return (
-        <h1>This is the account page</h1>
+        <body>
+            <h1>My Account</h1>
+            
+            <div>
+                <Button href={EDIT_ACCOUNT}>Edit Account</Button>
+            </div>
+
+            <div>
+                <img src={ require('./stock.jpg') } />
+            </div>
+
+        </body>
     )
 }
 const mapStateToProps = (state) => {
